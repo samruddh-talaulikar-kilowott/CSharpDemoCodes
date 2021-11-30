ceeef1ea0028b49452f39b6d71284f2f85710a9a
Samruddh Talaulikar <92842223+samruddh-talaulikar-kilowott@users.noreply.github.com>
2021-11-30 18:20:10 +0530

    Add files via upload

---
+++ Folder/Program.cs
@@ -0,0 +1,22 @@
+﻿//Main File
+using CSharpDemoCodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Program.cs\n");
+
+            Stats.MeanMedianMode();
+        }
+
+
+    }
+}

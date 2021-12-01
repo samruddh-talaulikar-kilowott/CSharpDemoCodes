8bea54648d8edbe3623e16eaeaab8f6ac2ea02e3
Samruddh Talaulikar <samruddh.talaulikar@kilowott.com>
2021-12-01 12:08:18 +0530

    Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Main File
+using CSharpDemoCodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Program.cs\n");
+
+            Stats.MeanMedianMode();
+        }
+
+
+    }
+}
